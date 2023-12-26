cfa97c2e693f93a3f3a036d38cc6ca098ed4d13b
Kurz <kurz956kk@gmail.com>
2023-12-26 10:05:23 +0500

    added forms

--- feedback/urls.py
+++ feedback/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, done
+from .views import index, done, update_feedback
 
 urlpatterns = [
     path('done', done),
-    path('', index),
+    path('', index, name='index'),
+    path('<int:id_feedback>', update_feedback),
 ]
--- feedback/views.py
+++ feedback/views.py
@@ -1,16 +1,44 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from  .forms import FeedbackForm
+from .models import Feedback
 
-
+from django.views import View
 # Create your views here.
+
+class FeedBackView(View):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
 def index(request):
     if request.method == 'POST':
-        name = request.POST['name']
-        if len(name) == 0:
-            return render(request, 'feedback/feedback.html', context={'got_error': True})
-        print(name)
-        return HttpResponseRedirect('/done')
-    return render(request, 'feedback/feedback.html', context={'got_error': False})
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
+            return HttpResponseRedirect('/done')
+        # name = request.POST['name']   # формы через ХТМЛ
+        # if len(name) == 0:
+        #     return render(request, 'feedback/feedback.html', context={'form': form})
+        # print(name)
+        # return HttpResponseRedirect('/done')
+    else:
+        form = FeedbackForm()
+    return render(request, 'feedback/feedback.html', context={'form': form})
+
+def update_feedback(request, id_feedback):
+    feed = Feedback.objects.get(id=id_feedback)
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST, instance=feed)
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
+            return HttpResponseRedirect(f'/{id_feedback}')
+    else:
+        form = FeedbackForm(instance=feed)
+    return render(request, 'feedback/feedback.html', context={'form': form})
 
 def done(request):
     return render(request, 'feedback/done.html')
